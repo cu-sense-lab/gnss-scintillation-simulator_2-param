e2330fddda6690a68010a7342cca57578ee28cb8
tapyu <rubem.engenharia@gmail.com>
2024-08-05 15:17:25 +0200

    fix: Bad geometry

--- Libraries/Utilities/ParaMapping.m
+++ Libraries/Utilities/ParaMapping.m
@@ -11,7 +11,7 @@ function [U_mapped,rhoFVeff_mapped] = ParaMapping(userInput)
 % written by Dongyang.Xu@colorado.edu
 
  S4_range = [0.5883, 0.6760, 0.7480, 0.8071, 0.8558, 0.8961, 0.9296, 0.9576, 0.9811, 1.0010];
- U_range = [0.75:0.25:3.0];
+ U_range = 0.75:0.25:3.0;
  a_log_range = [0.7841, 0.7361, 0.6985, 0.6693, 0.6440, 0.6231, 0.6027, 0.5874, 0.5724, 0.5574];
  U_mapped = interp1(S4_range,U_range,userInput.S4,'spline');
  a_mapped = interp1(U_range,a_log_range,U_mapped,'spline');
